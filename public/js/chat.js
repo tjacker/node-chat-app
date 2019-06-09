34815d3af49ccd7964d1e564a22b88f755817cf9
Tim Acker <tjacker31@gmail.com>
2019-06-09 17:53:42 -0400

    Disables form and share location until a response is received

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,18 +1,33 @@
 // Establishes client connection
 const socket = io();
 
+const messageForm = document.getElementById('message-form');
+const messageFormInput = messageForm.querySelector('input');
+const messageFormButton = messageForm.querySelector('button');
+const sendLocationButton = document.getElementById('send-location');
+
 socket.on('message', message => {
 	console.log(message);
 });
 
-document.getElementById('message-form').addEventListener('submit', e => {
+messageForm.addEventListener('submit', e => {
 	e.preventDefault();
+
+	// Disable form until a response is received
+	messageFormButton.disabled = true;
 
 	// Gets message from input field and emits message to the server
 	const message = e.target.elements.message.value;
 
 	// Callback function is sent to the server to determine if emit event was successful
 	socket.emit('sendMessage', message, error => {
+		// Re-enable form in callback
+		messageFormButton.disabled = false;
+
+		// Reset input field and restore focus
+		messageFormInput.value = '';
+		messageFormInput.focus();
+
 		if (error) {
 			return console.warn(error);
 		}
@@ -21,7 +36,11 @@ document.getElementById('message-form').addEventListener('submit', e => {
 	});
 });
 
-document.getElementById('send-location').addEventListener('click', () => {
+sendLocationButton.addEventListener('click', () => {
+	// Disable button until a response is received
+	// If browser does not support geolocation, the button will remain disabled
+	sendLocationButton.disabled = true;
+
 	if (!navigator.geolocation) {
 		return alert('Geolocation is not supported by your browser.');
 	}
@@ -30,6 +49,8 @@ document.getElementById('send-location').addEventListener('click', () => {
 		if (!position.coords) return;
 
 		const { latitude, longitude } = position.coords;
+		// Re-enable button in callback
+		sendLocationButton.disabled = false;
 
 		socket.emit('sendLocation', { latitude, longitude }, () => {
 			console.info('Location information shared successfully.');
